b37d931cdd4345fddabac24e6233b94ee143d486
Luis Geniole <luis.geniole@gmail.com>
2021-09-25 22:35:52 -0300

    Add missing tags.

--- src/pages/OGTags.tsx
+++ src/pages/OGTags.tsx
@@ -3,19 +3,25 @@ const OGTags = () => {
     <>
       <meta property="og:locale" content="en_US" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="Create React Mine!" />
+      <meta property="og:url" content="https://create-react-mine.vercel.app/" />
+      <meta property="og:site_name" content="Create React Mine!" />
+
+      <meta name="title" property="og:title" content="Create React Mine!" />
       <meta
+        name="description"
         property="og:description"
         content="A blockchain and Proof of Work mining simulator made with NextJS and Typescript."
       />
-      <meta property="og:url" content="https://create-react-mine.vercel.app/" />
-      <meta property="og:site_name" content="Create React Mine!" />
+
       <meta
+        name="image"
         property="og:image"
         content="https://create-react-mine.vercel.app/preview.png"
       />
       <meta property="og:image:width" content="2000" />
       <meta property="og:image:height" content="1600" />
+
+      <meta name="author" content="@librity" />
     </>
   )
 }
