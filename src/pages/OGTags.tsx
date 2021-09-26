5bbd13b9145921f111c0e6ed8c7d63fa160c77f4
Luis Geniole <luis.geniole@gmail.com>
2021-09-25 22:30:35 -0300

    Format.

--- src/pages/OGTags.tsx
+++ src/pages/OGTags.tsx
@@ -7,16 +7,13 @@ const OGTags = () => {
       <meta
         property="og:description"
         content="A blockchain and Proof of Work mining simulator made with NextJS and Typescript."
-        />
-      <meta
-        property="og:url"
-        content="https://create-react-mine.vercel.app/"
-        />
+      />
+      <meta property="og:url" content="https://create-react-mine.vercel.app/" />
       <meta property="og:site_name" content="Create React Mine!" />
       <meta
         property="og:image"
         content="https://create-react-mine.vercel.app/preview.png"
-        />
+      />
       <meta property="og:image:width" content="2000" />
       <meta property="og:image:height" content="1600" />
     </>
